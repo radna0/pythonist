3a94dac578731122a22f32aaf820f6ab10432228
HGB <87762857+radna0@users.noreply.github.com>
2024-04-05 17:51:14 +0700

    claen up

--- 2.5.py
+++ 2.5.py
@@ -1,5 +1,5 @@
 
-# # # And / Or / Not
+# # # And / Or / Not + TRUE/FALSE (BOOLEAN)
 
 
 # # # And
@@ -9,17 +9,17 @@
 # # # False and True = False
 # # # False and False = False
 
-x = int(input("so thu 1: "))
-y = int(input("so thu 2: "))
+# x = int(input("so thu 1: "))
+# y = int(input("so thu 2: "))
 
-if x > 0 and y > 0:
-    print("ca 2 so deu lon hon 0")
-elif x > 0 and y < 0:
-    print("so thu 1 lon hon 0, so thu 2 nho hon 0")
-elif x < 0 and y > 0:
-    print("so thu 1 nho hon 0, so thu 2 lon hon 0")
-else:
-    print("ca 2 so deu nho hon 0")
+# if x > 0 and y > 0:
+#     print("ca 2 so deu lon hon 0")
+# elif x > 0 and y < 0:
+#     print("so thu 1 lon hon 0, so thu 2 nho hon 0")
+# elif x < 0 and y > 0:
+#     print("so thu 1 nho hon 0, so thu 2 lon hon 0")
+# else:
+#     print("ca 2 so deu nho hon 0")
 
 
 # # # Or
@@ -30,38 +30,51 @@ else:
 # # # False or False = False
 
 
-x = int(input("mot so bat ky: "))
+# x = int(input("mot so bat ky: "))
 
-#  uu tien su dung cach nay
-#  code nay de hieu, ngan gon, it dieu kien hon
-#  chi dung else khi co nhieu truong hop khac nhau
-#  ma khong can phai xac dinh ro rang
-if x == 0:
-    print("so do bang 0")
-else:
-    print("so do khac 0")
+# #  uu tien su dung cach nay
+# #  code nay de hieu, ngan gon, it dieu kien hon
+# #  chi dung else khi co nhieu truong hop khac nhau
+# #  ma khong can phai xac dinh ro rang
+# if x == 0:
+#     print("so do bang 0")
+# else:
+#     print("so do khac 0")
 
-if x > 0 or x < 0:
-    print("so do khac 0")
-else:
-    print("so do bang 0")
-
-
-x = int(input("so thu 1: "))
-y = int(input("so thu 2: "))
-
-if x > 0 and y > 0:
-    print("ca 2 so deu duong")
-elif (x > 0 and y < 0) or (x < 0 and y > 0):
-    print("ca 2 so trai dau")
-else:
-    print("ca 2 so deu am")
+# if x > 0 or x < 0:
+#     print("so do khac 0")
+# else:
+#     print("so do bang 0")
 
 
-# # # Not
+# x = int(input("so thu 1: "))
+# y = int(input("so thu 2: "))
 
-# # # not True = False
-# # # not False = True
+# if x > 0 and y > 0:
+#     print("ca 2 so deu duong")
+# elif (x > 0 and y < 0) or \
+#         (x < 0 and y > 0):
+#     print("ca 2 so trai dau")
+# elif x == 0 and y == 0:
+#     print("ca 2 so deu bang 0")
+# else:
+#     print("ca 2 so deu am")
+
+
+# # # # Not
+
+# input = True/False
+# output = not(input)
+
+# # # # not True = False
+# # # # not False = True
+
+# # # # not not True = True
+# # # # not not False = False
+
+#  + * - = -
+#  - * - =  +
+
 
 x = int(input("so bat ky: "))
 
